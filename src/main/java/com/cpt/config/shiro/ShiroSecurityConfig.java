822bd55f7c1a7696341117ee46f8bfd3a1ac2572
sangyiwen456@126.com <yiwen.sang@RSMYJNB0352.group.redstar.corp>
2016-12-20 20:26:00 +0800

    dev2

--- src/main/java/com/cpt/config/shiro/ShiroSecurityConfig.java
+++ src/main/java/com/cpt/config/shiro/ShiroSecurityConfig.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import javax.servlet.Filter;
 
 import org.apache.shiro.session.mgt.SessionManager;
+import org.apache.shiro.spring.LifecycleBeanPostProcessor;
 import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSourceAdvisor;
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
@@ -170,5 +171,8 @@ public class ShiroSecurityConfig {
         shiroFilter.setFilterChainDefinitionMap(definitionsMap);
         return shiroFilter;
     }
-    
+    @Bean
+	public LifecycleBeanPostProcessor lifecycleBeanPostProcessor() {
+		return new LifecycleBeanPostProcessor();
+	}
 }
